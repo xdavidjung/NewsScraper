49e3a14d6e28adf5aef96a00e89aa2dabd397ad8
David Jung <davejung@cs.washington>
2012-07-14 11:04:02 -0700

    Not finished yet but have a good skeleton.

--- news-converter/src/main/scala/edu/washington/cs/knowitall/ConverterMain.scala
+++ news-converter/src/main/scala/edu/washington/cs/knowitall/ConverterMain.scala
@@ -1,8 +1,16 @@
 package edu.washington.cs.knowitall;
+import edu.washington.cs.knowitall.tool.chunk.OpenNlpChunker
 
 object ConverterMain {
   
   def main(args: Array[String]): Unit = {
+    
+    val chunker = new OpenNlpChunker
+    
+    val arg: String = getString
+    
+    val tokens = chunker.chunk(arg).toIndexedSeq
+    
     // read from standard input, parse as JSON object. 
     // chunk from "sent" field to get List[ChunkedToken].
 
