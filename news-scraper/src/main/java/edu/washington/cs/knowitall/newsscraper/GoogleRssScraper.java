b806ba89cca68e53aa32472d2bf6cc95d1c6fd58
David Jung (Work) <xdavidjung@gmail.com>
2012-08-31 17:38:20 -0700

    Initial commit, nothing done yet.

---
+++ news-scraper/src/main/java/edu/washington/cs/knowitall/newsscraper/GoogleRssScraper.java
@@ -0,0 +1,11 @@
+package edu.washington.cs.knowitall.newsscraper;
+
+public class GoogleRssScraper implements RssScraper {
+
+    public void scrape(boolean fetchData, boolean processData,
+            String sourceDir, String targetDir) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
